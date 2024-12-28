534baa02fcb788ec357d10865f9dd9e4ef1fcdb4
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-12-28 23:57:30 +0600

    Feed Back

--- src/components/MakeAppointment.tsx
+++ src/components/MakeAppointment.tsx
@@ -13,7 +13,7 @@ const MakeAppointment = () => {
           <p className="text-white text-6xl text-center my-4 font-poppins">
             You Owe Yourself This Moment
           </p>
-          <button className="bg-primary px-[56px] py-[17px]  text-white text-sm hover:opacity-80 duration-300 ">
+          <button className="bg-primary px-[56px] py-[17px]  text-white text-base hover:opacity-80 duration-300 ">
             Make An appointment
           </button>
         </div>
